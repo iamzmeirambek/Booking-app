4c0ab077773643b4564326052a6195a934c8f72d
iamzmeirambek <Iamzmeirambek@gmail.com>
2023-04-04 11:22:25 +0600

    created migrations

---
+++ database/migrations/2023_04_04_051737_create_reservations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('hotel_id')->constrained('hotels');
+            $table->foreignId('room_id')->constrained('rooms');
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('guestFirstName');
+            $table->string('guestlastName');
+            $table->date('CheckIn');
+            $table->date('CheckOut');
+            $table->integer('totalPrice');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
